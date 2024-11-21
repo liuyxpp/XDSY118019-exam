210f9f363de0ec52d5fecb7971f00371f34452c6
Yuxian <co_sin_o@163.com>
2024-11-21 20:15:02 +0800

    finish part 2

---
+++ finalexam-part2/code/find.py
@@ -0,0 +1,48 @@
+import re
+
+def find_name_value(folder_name):
+        '''Split the name of a data directory into a (name, value) tuple.
+
+        The format of ``folder_name``:
+
+            <name><value>
+
+        If the value is negative, it should be followed by a 'n'.
+
+        Examples:
+            ::
+
+                phi0.1          # should return 'phi', 0.1
+                xN14.2          # should return 'xN', 14.2
+                kappa0.5n       # should return 'kappa', -0.5
+
+        Args:
+            folder_name (str): the name of a :term:`data directory`.
+
+        Returns:
+            tuple: a tuple contains:
+
+                * name (str): variable name.
+                * value (float): value of the variable.
+        '''
+        pattern = '([-+]?\d*\.\d+|[-+]?\d+)'
+        rst = re.split(pattern, folder_name)
+        if len(rst) < 2:
+            return folder_name, None
+        name = rst[0]
+        valuestr = rst[1]
+        sign_str = ''
+        if len(rst) > 2:
+            sign_str = rst[2]
+        if sign_str == 'n':
+            value = '-' + valuestr
+        else:
+            value = valuestr
+
+        return name, float(value)
+
+if __name__ == '__main__':
+    examples = ['phi0.1_xN14.2_kappa0.5n', 'a1_b14n_n0_c0.2']
+    for example in examples:
+        print(find_name_value(example))
+        
